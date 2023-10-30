98f1be3113534b14c7c9bf73245be80fff6f9195
Pabloglezr8 <pabligoru99@gmail.com>
2023-10-30 09:49:38 +0100

    hacemos la parte del js

---
+++ scripts/index.js
@@ -0,0 +1,82 @@
+window.addEventListener("load", inicioSesion);
+
+let saldo = 1000;
+let tries = 3;
+const correctPswrd = "1234";
+
+function mostrarSaldo() {
+    const message = document.getElementById("message")
+    message.textContent = `Su saldo es: ${saldo}`
+}
+
+function inicioSesion() {
+    const pswrd = prompt("Ingrese la contraseña: ")
+
+    while (tries > 1 && pswrd !== correctPswrd) {
+        tries--
+        alert(`Constraseña incorrecta le quedan ${tries}`)
+        prompt("Ingrese la contraseña: ")
+
+    }
+
+    if (pswrd !== correctPswrd) {
+        window.location.replace("/templates/bloqueado.html")
+    } else {
+        alert("Se ha iniciado sesión con éxito")
+        mostrarSaldo()
+    }
+
+}
+
+
+function depositar() {
+    const depositar = parseFloat(prompt("Cantidad a depositar: "))
+
+    if (isNaN(depositar) || depositar <= 0) {
+        alert("Cantidad no válida")
+    } else {
+        saldo += depositar;
+        mostrarSaldo()
+    }
+}
+
+function retirar() {
+    const retirar = parseFloat(prompt("Cantidad a retirar: "))
+
+    if (isNaN(retirar) || retirar <= 0 || retirar > saldo) {
+        alert("Cantidad no válida")
+    } else {
+        saldo -= retirar;
+        mostrarSaldo()
+    }
+}
+
+function transferir() {
+    const transferir = parseFloat(prompt("Cantidad a transferir: "))
+
+    if (isNaN(transferir) || transferir <= 0 || transferir > saldo) {
+        alert("Cantidad no válida")
+    } else {
+        const cuenta = prompt("Numero de cuneta: ")
+
+        if (/^ES\d{22}$/.test(cuenta) === false) {
+            alert("Numero de cuneta no válido")
+        } else {
+            saldo -= transferir;
+            mostrarSaldo()
+        }
+    }
+}
+
+function changePswrd() {
+    const pswrd = prompt("Ingrese su contraseña: ")
+    
+    if (pswrd === correctPswrd) {
+        const newPswrd = prompt("Nueva contraseña: ")
+        correctPswrd = newPswrd
+        console.log(correctPswrd)
+    }else{
+        alert("Contraseña incorrecta. No se puede cambiar la contraseña")
+    }
+
+}
